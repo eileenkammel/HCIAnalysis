23306596f011f58c635284c4025e19276b2927f3
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2023-08-31 12:43:20 +0200

    add sorted transcripts + sorting script

---
+++ sort_transcripts.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+# Couldn't find a way to export transcripts
+# sorted by time stamps directly from Elan
+# This script sorts the transcripts by time stamps
+# and overwrites the unsorted files
+# Result: Utterances are chronologically ordered in the csv files
+
+
+path_base = "/Users/eileen/HCIAnalysis/Transcripts"
+
+for file in os.listdir(path_base):
+    # check if file is a csv file, just in case
+    if file.endswith(".csv"):
+        path = os.path.join(path_base, file)
+        # read transcript from file , add header
+        df = pd.read_csv(path, sep=",", header=None, names=[
+                         "speaker", "start", "end", "duration", "text"])
+        # sort df by column "start"
+        df.sort_values(by="start", inplace=True)
+        # reset index
+        df.reset_index(drop=True, inplace=True)
+        # save sorted df to file (overwrite unsorted file)
+        df.to_csv(path, index=False)
