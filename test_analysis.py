c0e8ed10f587a4444130174d1db574f5ca2329d7
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2023-09-03 15:09:50 +0200

    change import formatting

--- test_analysis.py
+++ test_analysis.py
@@ -1,6 +1,11 @@
 import pytest
 import pandas as pd
-from analysis import *
+from analysis import (
+    count_words, count_participant_utterances, count_furhat_utterances,
+    count_disfluencies, get_disfluency_distribution,
+    get_mean_words_per_utterance, get_mean_dfs_per_utterance,
+    get_mean_dfs_per_word, total_talk_duration
+)
 
 
 @pytest.fixture(scope="module")
@@ -24,7 +29,7 @@ class TestAnalysis:
 
     def test_get_disfluency_distribution(self, test_df):
         assert get_disfluency_distribution(
-            test_df) == {'FP': 8, 'SUB': 2, 'UP': 3, 'REP': 1}
+            test_df) == {"FP": 8, "SUB": 2, "UP": 3, "REP": 1}
 
     def test_get_mean_words_per_utterance(self, test_df):
         words = count_words(test_df)
@@ -40,9 +45,11 @@ class TestAnalysis:
         disfluencies = count_disfluencies(test_df)
         words = count_words(test_df)
         assert get_mean_dfs_per_word(disfluencies, words) == 0.069
-    
+
     def test_total_talk_duration(self, test_df):
         test_df["start"] = pd.to_timedelta(test_df["start"])
         test_df["end"] = pd.to_timedelta(test_df["end"])
         test_df["duration"] = pd.to_timedelta(test_df["duration"])
-        assert total_talk_duration(test_df, 2) == {'participant_no': 2, 'participant_talk': pd.Timedelta('0 days 00:01:22.101000'), 'furhat_talk': pd.Timedelta('0 days 00:03:51.247000')}
+        assert total_talk_duration(test_df, 2) == {"participant_no": 2, "participant_talk": pd.Timedelta(
+            "0 days 00:01:22.101000"),
+            "furhat_talk": pd.Timedelta("0 days 00:03:51.247000")}
