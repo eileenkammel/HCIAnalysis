c95e085a69b45c316db2e598f04bb64b59c08a14
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2023-09-05 13:33:56 +0200

    add file for all data concatenated

--- sort_transcripts.py
+++ sort_transcripts.py
@@ -10,16 +10,46 @@ import os
 
 path_base = "/Users/eileen/HCIAnalysis/Transcripts"
 
-for file in os.listdir(path_base):
-    # check if file is a csv file, just in case
-    if file.endswith(".csv"):
-        path = os.path.join(path_base, file)
-        # read transcript from file , add header
-        df = pd.read_csv(path, sep=",", header=None, names=[
-                         "speaker", "start", "end", "duration", "text"])
-        # sort df by column "start"
-        df.sort_values(by="start", inplace=True)
-        # reset index
-        df.reset_index(drop=True, inplace=True)
-        # save sorted df to file (overwrite unsorted file)
-        df.to_csv(path, index=False)
+
+def sort_transcripts(path_base):
+    # iterate over all files in path
+    for file in os.listdir(path_base):
+        # check if file is a csv file, just in case
+        if file.endswith(".csv"):
+            path = os.path.join(path_base, file)
+            # read transcript from file , add header
+            df = pd.read_csv(path, sep=",", header=None, names=[
+                            "speaker", "start", "end", "duration", "text"])
+            # sort df by column "start"
+            df.sort_values(by="start", inplace=True)
+            # reset index
+            df.reset_index(drop=True, inplace=True)
+            # save sorted df to file (overwrite unsorted file)
+            df.to_csv(path, index=False)
+
+# Concatenate all transcripts into one file, add a column for participant number
+# and save to file
+# Result: One csv file with all transcripts, sorted by time stamps
+# and with a column for participant number
+
+# create empty df
+
+
+def concat_all_transcripts(path_base):
+    df_all = pd.DataFrame(columns=["Participant No", "speaker", "start",
+                                    "end", "duration", "text"])
+    # iterate over all files in path
+    for participant in range(2, 8):
+        path = os.path.join(path_base, "p" +
+                            str(participant)+"_transcript.csv")
+        df = pd.read_csv(path, sep=",", header=0)
+        # extract participant number from file name
+        # add column with participant number
+        df["Participant No"] = participant
+        # concat df to df_all
+        df_all = pd.concat([df_all, df])
+
+    # save df_all to file
+    df_all.to_csv("Transcripts/all_transcripts.csv", index=False)
+
+concat_all_transcripts(path_base)
